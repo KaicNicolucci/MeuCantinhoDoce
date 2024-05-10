52e561ebb062404886f2b7a03f22156afabca295
Kaic Nicolucci <kaic.nicolucci@gmail.com>
2024-05-10 15:42:50 -0300

    version 1.0.2

--- src/app/components/adicionarCarrinho.js
+++ src/app/components/adicionarCarrinho.js
@@ -1,6 +1,7 @@
 import { Produto } from "../modules/produto.js";
 import { carrinhoView } from "../modules/carrinho-view.js";
 
+
 let carrinho = [];
 
 export function adicionarCarrinho(id, nome, valor, img) {
@@ -10,5 +11,17 @@ export function adicionarCarrinho(id, nome, valor, img) {
     carrinho.push(produto);
 
     carrinhoView(carrinho);
+    setTimeout(popUp(), 2000)
+
+}
+
+function popUp() {
+
+    const modal = document.getElementById('modal');
+    modal.style.display = 'block';
+
+    const closeButton = document.querySelector('.close');
+    closeButton.addEventListener('click', function(){
+        modal.style.display = 'none';})
 
 }
